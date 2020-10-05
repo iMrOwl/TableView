963c4f97b1cfcfedbf67fa9d8cc895831411a3c6
Сычев Евгений Дмитриевич <ydsychev@mts.ru>
2020-10-05 16:27:29 +0300

    Initial Commit

---
+++ TableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableView
+//
+//  Created by Сычев Евгений Дмитриевич on 05.10.2020.
+//  Copyright © 2020 Сычев Евгений Дмитриевич. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
